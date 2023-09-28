96d8c594425e4fa81a6110572bedae3c88fccc37
Aamrin Islam <aamrinislam.Dev@gmail.com>
2023-09-28 16:54:45 +0530

    Update sayTheDigit.cpp

--- Recursion/sayTheDigit.cpp
+++ Recursion/sayTheDigit.cpp
@@ -8,7 +8,9 @@ void sayDigit(int n, string arr[]){
     int lastDigit = n%10;
     n=n/10;
     // cout<<arr[lastDigit]<<" "; instead of printing four one two it will print two one four 
+    
     sayDigit(n,arr);//recursive call for rest of the number except the last digit
+    
     cout<<arr[lastDigit]<<" "; //comparing the digit with array value and return the value After completing all the recursive call then it will print the values else it will print in reverse way
                                
 }
