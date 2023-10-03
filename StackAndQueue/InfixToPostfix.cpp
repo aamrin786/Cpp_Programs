5cd7b3048efe936dfe706fa971a001014d0eae6e
aamrin786 <aamrinislam.Dev@gmail.com>
2023-10-03 18:18:49 +0530

    InfixToPostfix

---
+++ StackAndQueue/InfixToPostfix.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+  string infixToPostfix(string s) {
+        unordered_map<char,int>mp;
+        int n = s.size();
+        stack<char>st;
+        string str="";
+        
+        mp['^']=3;
+        mp['*']=2;
+        mp['/']=2;
+        mp['+']=1;
+        mp['-']=1;
+        
+        for(int i=0;i<n;i++){
+            if(s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z'||s[i]>='0'&&s[i]<='9'){
+                str+=s[i];
+            }
+//Handling the parenthesis
+        if(s[i]=='('){
+            st.push(s[i]);
+        }
+        else if(s[i]==')'){
+            while(st.top()!='(' ){
+                str+=st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+
+// handling the expressions w/o characters
+        else if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/' || s[i]=='^'){
+            while( !st.empty()&&mp[st.top()]>=mp[s[i]]){
+                str+=st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+        }
+        
+        while(!st.empty()){
+            str+=st.top();
+            st.pop();
+        }
+        return str;
+    }
