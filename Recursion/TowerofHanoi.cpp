61a2cc7069b5e6a96fab438386dfe8f4fc57a810
aamrin786 <aamrinislam.Dev@gmail.com>
2023-10-08 22:15:47 +0530

    updated TowerofHanoi

--- Recursion/TowerofHanoi.cpp
+++ Recursion/TowerofHanoi.cpp
@@ -10,10 +10,19 @@ long long toh(int N, int from, int to, int aux) {
             cout<<"move disk 1 from rod "<<from<<" to rod "<<to<<endl;
             return 1;
         }
-        moves+=toh(N-1,from,aux,to);
+        moves+=toh(N-1,from,aux,to); // every function call contains one move that's why moves+=
         cout<<"move disk "<<N<<" from rod "<<from<<" to rod "<<to<<endl;
         moves++;
         moves+=toh(N-1,aux,to,from);
 
         return moves;
 }
+
+/*Input:
+N = 2
+Output:
+move disk 1 from rod 1 to rod 2
+move disk 2 from rod 1 to rod 3
+move disk 1 from rod 2 to rod 3
+3*/
+
